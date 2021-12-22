561d034c07a4a420b712e41d0032c965ece58792
Dummyy-Project <stackuptech72@gmail.com>
2021-12-22 11:38:57 +0500

    changes in post model to cause conflict

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
 
 validates :title, :body, :presence => true
 #validates :body, :length => { :minimum => 4 }, :allow_blank => true
-validates :title, :length => { :minimum => 8 }
+validates :title, :length => { :minimum => 7 }
 validates :title, :uniqueness => true
 
 end
